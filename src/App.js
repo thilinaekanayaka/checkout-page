a44e00701af36fd0d765e354576f6da2269b7752
Thilina Ekanayaka <thilinaekanayaka@Thilinas-MacBook-Pro.local>
2023-03-08 15:48:15 +0800

    Item component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,26 @@ import './App.css';
 import Item from './components/Item';
 
 const App = () => {
+  const addItem = (id) => {
+    console.log('item added to cart :', id)
+  }
+
   return (
     <Fragment>
-      <Item test={"test text"} />
+      <Item
+        id={1}
+        name={`Kone`}
+        price={3488.99}
+        offer={`When 3 or more Kone is purchased, you’ll get a discount where the price is dropped to RM 2588.99 per unit`}
+        addItem={addItem}
+      />
+      <Item
+        id={2}
+        name={'Ironhide Cartridge'}
+        price={529.99}
+        offer={``}
+        addItem={addItem}
+      />
     </Fragment>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect, Fragment } from "react";
+import './Item.css';
 
 const Item = (props) => {
   return (
     <Fragment>
-      item desc : {props.test}
+      <div>{props.name}</div>
+      <div>RM {props.price}</div>
+      <div>Offer: {props.offer}</div>
+      <div><button onClick={() => props.addItem(props.id)}>Add to cart</button></div>
     </Fragment>
   )
 }
