e814a48b98ca335524bf549d74fe3d224522a75a
Thilina Ekanayaka <thilinaekanayaka@Thilinas-MacBook-Pro.local>
2023-03-08 19:23:03 +0800

    Add to cart logic

--- src/App.js
+++ src/App.js
@@ -17,6 +17,12 @@ const items = [
     price: 529.99,
     offerID: 0
   },
+  {
+    id: 3,
+    name: `Ironhide`,
+    price: 3299.99,
+    offerID: 0
+  },
 ]
 
 // Demo purposes only. Ideally should come from a backend API that sends a JSON of offer details.
@@ -28,14 +34,32 @@ const offers = [
   }
 ]
 
-const App = () => {
-  const [cart, setCartItems] = useState([]);
+// --- find
+// let itemDetails = items.find(item => item.id === 1);
+// console.log('itemDetails', itemDetails);
 
-  // let itemDetails = items.find(item => item.id === 1);
-  // console.log('itemDetails', itemDetails);
+// -- remove
+// const itemDetails = items.filter(item => item.id !== 2)
+// console.log('itemDetails', itemDetails);
+
+const App = () => {
+  const [cart, setCartItems] = useState([])
+  console.log('cart items', cart)
 
   const addItem = id => {
-    console.log('item added to cart :', id)
+    const itemIndexInCart = cart.findIndex(item => item.id === id)
+    if (!itemIndexInCart) {
+      const updatedItem = Object.assign({}, cart[itemIndexInCart]);
+      updatedItem.itemCount = updatedItem.itemCount + 1
+      const newCart = cart.slice()
+      newCart[itemIndexInCart] = updatedItem
+      setCartItems(newCart)
+    }
+    else {
+      const itemDetails = items.find(item => item.id === id)
+      itemDetails.itemCount = 1
+      setCartItems(cart => [...cart, itemDetails])
+    }
   }
 
   const removeItem = id => {
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,11 +2,15 @@ import React, { Fragment } from "react";
 import './Cart.css';
 
 const Cart = (props) => {
-  console.log('cart', props.cart)
+  // console.log('cart props', props.cart)
+
   return (
     <Fragment>
-      {props.cart.length === 0 &&
-        <div>- cart empty -</div>
+      {props.cart.length === 0 ?
+        <div>- cart empty -</div> :
+        <div>
+          cart not empty
+        </div>
       }
     </Fragment>
   )
